538326a362878a987cb0803dc8838cc8497fb757
Maxime Coriton <maxime.coriton@gmail.com>
2022-11-18 10:03:40 +0100

    calendrier

---
+++ src/conts.js
@@ -0,0 +1,8 @@
+export const DAYS = ["Lu", "Ma", "Me", "Je", "Ve", "Sa", "Di"];
+
+export const MOCKAPPS = [
+  { date: new Date(2022, 10, 3), title: "appointment", color: "#446554" },
+  { date: new Date(2022, 10, 6), title: "doctos", color: "#708898" },
+  { date: new Date(2022, 10, 25), title: "bd", color: "#047106" },
+  { date: new Date(2022, 10, 3), title: "second", color: "#371395" }
+];
